5f31ce16fb86634706b11e7d347f0ae6e4a77e4f
Michele <gerunmic@students.zhaw.ch>
2015-04-27 17:20:32 +0200

    Remove field "Occurency" and check on uniqueness

--- Models/LangElement.cs
+++ Models/LangElement.cs
@@ -1,4 +1,4 @@
-﻿using System.Data.Entity;
+using System.Data.Entity;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Globalization;
@@ -13,18 +13,15 @@ namespace Refma.Models
     {
         
         public int ID { get; set; }
-        [Index("IX_UQ_ELEMENT", Order = 10, IsUnique = true)]
+      //  [Index("IX_UQ_ELEMENT", Order = 10, IsUnique = true)]
         public int LangId { get; set; }
         [ForeignKey("LangId")]
         public virtual Lang Lang { get; set; }
         [Required, MinLength(1), MaxLength(450)]
-        [Index("IX_UQ_ELEMENT", Order=20, IsUnique=true)]
+    //    [Index("IX_UQ_ELEMENT", Order=20, IsUnique=true)]
         public string Value { get; set; }
-        
-        public int Occurrency { get; set; }
+      
 
-        [NotMapped]
-        public bool Ignore { get; set; }
 
         public override bool Equals(object obj)
         {
